a6b0a964944d3b7d6b5cff041197efc564af19f9
sherry <95038227+swiftiecoder@users.noreply.github.com>
2024-04-07 16:53:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,3 @@ def index():
         return Response('ok', status=200)
     else:
         return "<h1>Something went wrong</h1>"
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=5000)
